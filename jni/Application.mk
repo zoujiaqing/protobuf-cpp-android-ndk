af15a2d70551a85c3a0aec76c4646572eb771ecc
zoujiaqing <zoujiaqing@gmail.com>
2020-08-27 16:56:22 +0800

    Add protobuf 3.13.0 source code for Android.

---
+++ jni/Application.mk
@@ -0,0 +1,11 @@
+APP_STL := c++_static
+
+NDK_TOOLCHAIN_VERSION := 4.9
+#NDK_TOOLCHAIN_VERSION := clang
+
+APP_PLATFORM := android-19
+APP_ABI := arm64-v8a x86_64
+
+#CXXFLAGS = -c -O3
+APP_OPTIM := release
+
